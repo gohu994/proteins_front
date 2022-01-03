6a21ce10f503af8b469fa4e6f01f7b336b162459
ben_h <79261542+gohu994@users.noreply.github.com>
2022-01-03 20:25:37 +0100

    mise en place input recherche et submit

--- src/components/Recherche.js
+++ src/components/Recherche.js
@@ -1,31 +1,42 @@
 import React from "react"
-import {Select, FormControl, MenuItem} from "@mui/material";
+import {Select, FormControl, MenuItem, TextField, Button} from "@mui/material";
+import axios from "axios";
 
 function Recherche(props) {
 
-    const [protein,setProtein] = React.useState([0]);
+    const [protein,setProtein] = React.useState([]);
 
 
-    const handleChange= (event) => {
+    const handleChangeProtein = (e) => {
         props.setUrl("bolt://localhost:7687")
-        setProtein(event.target.value);
-
+        setProtein(e.target.value);
     };
 
+    const handleSubmitResearch = (e) => {
+        e.preventDefault();
+        const protSearch = {
+            body: { name: protein },
+            headers: {
+                "Content-Type": "application/json"
+            },
+            method: "POST"
+        };
+        axios.post("http://localhost:5000/protein", protSearch)
+    }
     return(
         <FormControl sx={{ m: 1, minWidth: 200 }}>
-            <Select
+            <TextField
+                autoFocus
+                margin="dense"
+                id="cherche"
+                label="cherche"
+                type="text"
+                variant="standard"
+                fullWidth
                 value={protein}
-                onChange={handleChange}
-                displayEmpty
-                defaultValue={0}
-                inputProps={{ 'aria-label': 'Without label' }} onClick={console.log(props.options[protein].val)}
-            >
-                {props.options.map((p,index) => {
-                    return <MenuItem value={index} key={p.key}> {p.val}</MenuItem>
-                })}
-            </Select>
-
+                onChange={handleChangeProtein}
+            />
+            <Button type="submit" onClick={handleSubmitResearch}>Search</Button>
         </FormControl>
 
     )
